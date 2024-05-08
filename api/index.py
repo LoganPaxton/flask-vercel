881801a7f27758407c3b44ff797241c4eceb35b6
Logan Paxton <111173907+LoganPaxton@users.noreply.github.com>
2024-05-08 12:10:17 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+static_path = '/static'
+template_path = '/templates'
+
+app = Flask(__name__, static_url_path=static_path, template_folder=template_path)
 
 @app.route('/')
 def index():
