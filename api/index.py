cb890e2863fd8c606302ad504d92a920f1e153b4
Logan Paxton <111173907+LoganPaxton@users.noreply.github.com>
2024-05-08 12:13:20 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,3 +8,7 @@ app = Flask(__name__, static_url_path=static_path, template_folder=template_path
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.errorhandler(500)
+def internal_server_err(e):
+    return "Internal Server Error (500) ", 500 
